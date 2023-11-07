18f84a7fc267ce3ab2b0e88e44154956c4c6b060
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2023-11-07 11:46:49 +0530

    functional command

--- prim.js
+++ prim.js
@@ -15,22 +15,24 @@ const client = new Client({
   });
 
 client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-const commandsPath = path.join(__dirname, 'commands')
-// console.log(commandsPath)
-const commandFiles =fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
 	}
 }
-
+  
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
@@ -52,8 +54,6 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
-
-
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
