606baf50cfeec303a40eb7f33e717369ce129ea7
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2023-11-30 12:28:31 +0530

    dropdown added, storing selected value

--- commands/invite/inviteCommand.js
+++ commands/invite/inviteCommand.js
@@ -1,23 +1,14 @@
-const { SlashCommandBuilder } = require('discord.js')
-const { Client, GatewayIntentBits } = require('discord.js')
+const { SlashCommandBuilder } = require('discord.js');
+const { inviteCommandEmbed } = require('./inviteMisc/inviteEmbed')
 
-const { inviteCommandEmbed } = require("./inviteMisc/inviteEmbed")
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
-  ]
-})
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('invite')
-    .setDescription('Send Invite for specified users'),
+        .setName('invite')
+        .setDescription('Send Invite for specified users'),
 
-    async execute (interaction) {
-        console.log("/invite command has been executed")
-        await inviteCommandEmbed(interaction, client)
+    async execute(interaction) {
+        console.log("/invite command has been executed");
+        // Embed for Invite
+        await inviteCommandEmbed(interaction)
     }
 }
--- commands/invite/inviteMisc/inviteEmbed.js
+++ commands/invite/inviteMisc/inviteEmbed.js
@@ -1,19 +1,58 @@
-const { EmbedBuilder } =require('discord.js')
+const { ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
 
+let selectedGame =[]
+console.log(selectedGame)
 module.exports = {
-    inviteCommandEmbed: async(interaction, client) => {
-        try{
-            const embed = new EmbedBuilder()
-            .setColor("#FF0000")
-            .setTitle("Invitation!")
-            .setDescription("Invitation description for testing")
-            .addFields({
-                name: "Invite", value : "Value"
-            })
-            await interaction.reply({embeds :[embed], ephemeral: true})
+    inviteCommandEmbed: async (interaction, client) => {
+        try {
+            const menu = new ActionRowBuilder()
+                .addComponents(
+                    new StringSelectMenuBuilder()
+                        .setCustomId('select_game')          
+                        .setPlaceholder("Select Game")
+                        .addOptions([
+                            {
+                                label: "Valorant",
+                                description: "5v5 PvP",
+                                value: "VALO"
+                            },
+                            {
+                                label: "Minecraft",
+                                description: "Survival Sandbox",
+                                value: "MC"
+                            },
+                            {
+                                label: "GTA V",
+                                description: "Open-World",
+                                value: "GTA V"
+                            }
+                        ])
+                );
+            await interaction.reply({ content: "Select Game", components: [menu] });
+
+            // Collect the interaction for the select menu
+            const collector = interaction.channel.createMessageComponentCollector({ componentType: 'SELECT_MENU', time: 15000 });
+
+            collector.on('collect', async (selectInteraction) => {
+                if (selectInteraction.isSelectMenu()) {
+                    const selectedOption = selectInteraction.values[0]; // Assuming only one value can be selected
+                    selectedValues.push(selectedOption); // Store the selected value in the array
+                    await selectInteraction.reply(`You selected ${selectedOption}`);
+                    // You can perform further processing here based on the selectedOption value
+                }
+            });
+
+            collector.on('end', (collected) => {
+                console.log(`Collected ${collected.size} interactions`);
+                // You can perform additional actions or post-process the collected values here if needed
+            });
+
         } catch (error) {
-        console.error('Error fetching commands:', error)
-        ainteraction.reply({ content: 'Error fetching commands.', ephemeral: true })
-      }
-  }
-}
+            console.error('Error fetching commands:', error);
+            interaction.reply({ content: 'Error fetching commands.', ephemeral: true });
+        }
+    },
+    getSelectedValues: () => {
+        return selectedValues;
+    }
+};
