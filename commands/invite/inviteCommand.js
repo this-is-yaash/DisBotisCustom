0bf779a9f375f5c440afb21b3ca060e8f5449f43
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2023-12-08 19:28:28 +0530

    alternate approach

--- commands/invite/inviteCommand.js
+++ commands/invite/inviteCommand.js
@@ -1,5 +1,8 @@
-const { SlashCommandBuilder } = require('discord.js');
-const inviteEmbed = require('./inviteMisc/inviteEmbed');
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, EmbedBuilder, StringSelectMenuOptionBuilder, ComponentType } = require('discord.js');
+
+const config = require('./gameList.json');
+const games = config.games
+
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,7 +10,52 @@ module.exports = {
         .setDescription('Send Invite for specified users'),
 
     async execute(interaction, client, users) {
-        console.log("/invite command has been executed");
-        await inviteEmbed.inviteCommandEmbed(interaction, client, users); // Pass 'client' to inviteCommandEmbed
+        // Check permissions
+        if (!interaction.member.permissions.has('SEND_MESSAGES')) {
+            return await interaction.reply({ content: 'You don\'t have permission to use this command!', ephemeral: true });
+        }    
+        // Game selection
+        const gameSelectionRow = new ActionRowBuilder()
+            .addComponents(
+            new StringSelectMenuBuilder()
+                .setCustomId('game_selection')
+                .setPlaceholder('Select a game')
+                .addOptions(
+                games.map((game) =>
+                    new StringSelectMenuOptionBuilder()
+                    .setLabel(game.label)
+                    .setDescription(game.description)
+                    .setValue(game.value)
+                )
+                )
+            );
+        // Send initial embed with game selection menu
+        const initialEmbed = new EmbedBuilder()
+            .setTitle('Choose a Game')
+            .setDescription('Please select the game you want to invite members to play.');
+        
+        await interaction.reply({ embeds: [initialEmbed], components: [gameSelectionRow] });
+        
+        // Store selected game on interaction object
+        interaction.selectedGame = null;  
+        const collector = interaction.channel.createMessageComponentCollector({
+            componentType: ComponentType.SELECT_MENU,
+            filter: (i) => i.user.id === interaction.user.id && i.customId === 'game_selection',
+            time: 10000,
+        });
+        
+        // Collect selected game
+        collector.on('collect', async (collected) => {
+            interaction.selectedGame = games.find((game) => game.value === collected.values[0]); // Store selected game object
+            
+            // Create user selection menu
+            const userSelectionMenu = await interaction.client.commands.get('inviteUsers').createUserMenu(interaction);
+          
+            // Edit reply with instructions and user selection menu
+            await interaction.editReply({ content: `Select users to invite for ${interaction.selectedGame.label}:`, components: [userSelectionMenu] });
+          
+            // **Initialize userCollector inside collect event**
+            const userCollector = interaction.client.commands.get('inviteUsers').createUserCollector(interaction);
+        });
     }
 };
---
+++ commands/invite/inviteUsers.js
@@ -0,0 +1,37 @@
+const { ActionRowBuilder, UserSelectMenuBuilder, ComponentType } = require('discord.js');
+
+module.exports = {
+    createUserMenu: async (interaction, selectedValue) => {
+        const userSelect = new ActionRowBuilder()
+            .addComponents(
+                new UserSelectMenuBuilder()
+                    .setCustomId('users')
+                    .setPlaceholder('Select multiple users.')
+                    .setMinValues(1)
+                    .setMaxValues(5)
+            );
+
+        return userSelect;
+    },
+    createUserCollector: (interaction) => {
+        const userCollector = interaction.channel.createMessageComponentCollector({
+            componentType: ComponentType.SELECT_MENU,
+            filter: (i) => i.user.id === interaction.user.id && i.customId === 'users',
+            time: 10000,
+        });
+        const selectedUsers = []; // Array to store selected user IDs
+        userCollector.on('collect', (userInteraction) => {
+            const userIds = userInteraction.values; // Get the selected user IDs
+            userIds.forEach((userId) => {
+                selectedUsers.push(userId); // Store the selected user IDs
+            });
+        });
+
+        userCollector.on('end', (collected) => {
+            // Perform any additional actions after user selection ends
+            console.log(`Collected ${selectedUsers.length} user IDs: ${selectedUsers}`);
+        });
+
+        return userCollector;
+    }
+};
